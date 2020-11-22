f2f7ecad9d842f682ca8081084cd3afbc903d887
Ryan Block <rblock@gmail.com>
2020-11-22 13:41:15 -0800

    Update deps, remove deprecated utils call

--- src/shared/copy-static-json.js
+++ src/shared/copy-static-json.js
@@ -3,7 +3,6 @@ let { existsSync } = require('fs')
 let { join } = require('path')
 let series = require('run-series')
 let print = require('../_printer')
-let { readArc } = require('@architect/utils')
 
 /**
  * copies public/static.json
@@ -15,17 +14,13 @@ let { readArc } = require('@architect/utils')
  * else     | vendor/shared/static.json
  */
 module.exports = function copyStatic (params, paths, callback) {
-  let { update, only } = params
+  let { inventory, update, only } = params
+  let { get } = inventory
   let go = !only || only === 'staticJson' || only === 'shared'
 
-  let { arc } = readArc()
-  let staticDir = 'public'
-  if (arc.static && arc.static.some(i => i[0] === 'folder')) {
-    staticDir = arc.static[arc.static.findIndex(i => i[0] === 'folder')][1] || 'public'
-  }
-  let static = join(process.cwd(), staticDir, 'static.json')
-  let hasStatic = existsSync(static)
-
+  let staticDir = get.static('folder')
+  let static = staticDir && join(process.cwd(), staticDir, 'static.json')
+  let hasStatic = staticDir && existsSync(static)
   if (hasStatic && go) {
     // Kick off logging
     let done = `Hydrated app with static.json`
