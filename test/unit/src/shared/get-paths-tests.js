883b64147ee7ba67372f212c2c22db2c085d5b45
filmaj <maj.fil@gmail.com>
2021-04-23 10:34:20 -0400

    bump mock-fs to 4.14, eliminating tape+mock-fs bug in tests

--- test/unit/src/shared/get-paths-tests.js
+++ test/unit/src/shared/get-paths-tests.js
@@ -1,8 +1,6 @@
 let test = require('tape')
-let { join } = require('path')
 let mockFs = require('mock-fs')
-// PUBLIC SERVICE ANNOUNCEMENT!
-// if youre going to edit these tests, always remember to mockFs.restore() BEFORE you assert with tape because https://github.com/tschaub/mock-fs/issues/201
+let { join } = require('path')
 let getPaths = require('../../../../src/shared/get-paths')
 let lambdaPath = join('app', 'src', 'http', 'get-items')
 let lambdaPathMockFs = lambdaPath.replace(/\\/g, '/') // mock-fs expects unix paths, even on windows :|
@@ -24,8 +22,8 @@ test('inventory with a shared path to hydrate and a single node runtime lambda a
     }
   }
   let paths = getPaths(inventory)
-  mockFs.restore()
   t.equals(paths[lambdaPath], join(lambdaPath, 'node_modules', '@architect'), 'node runtime lambda returns node_modules path')
+  mockFs.restore()
 })
 
 test('inventory with a shared path to hydrate and a single non-node runtime lambda aliased to a single directory should return a path to vendor', t => {
@@ -46,8 +44,8 @@ test('inventory with a shared path to hydrate and a single non-node runtime lamb
     }
   }
   let paths = getPaths(inventory)
-  mockFs.restore()
   t.equals(paths[lambdaPath], join(lambdaPath, 'vendor'), 'non-node runtime lambda returns vendor path')
+  mockFs.restore()
 })
 
 test('inventory with a shared path to hydrate and multiple node runtime lambdas aliased to a single directory should return a single path to node_modules', t => {
@@ -69,6 +67,6 @@ test('inventory with a shared path to hydrate and multiple node runtime lambdas 
     }
   }
   let paths = getPaths(inventory)
-  mockFs.restore()
   t.equals(paths[lambdaPath], join(lambdaPath, 'node_modules', '@architect'), 'node runtime lambda returns node_modules path')
+  mockFs.restore()
 })
