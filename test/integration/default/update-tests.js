2d0a1eb66bced7a91791845fc94399dcfcfc8d85
Ryan Block <rblock@gmail.com>
2023-03-09 15:20:47 -0800

    Fix tests broken by `lockFileVersion` 3

--- test/integration/default/update-tests.js
+++ test/integration/default/update-tests.js
@@ -21,9 +21,7 @@ test(`[Default (file copying)] update() bumps installed dependencies to newer ve
       else {
         // eslint-disable-next-line
         let pkgLock = JSON.parse(readFileSync(join(mockTmp, nodeFunctions[0], 'package-lock.json')))
-        // TODO ↓ remove me! ↓
-        console.log(`pkgLock:`, JSON.stringify(pkgLock, null, 2))
-        let newVersion = pkgLock.lockfileVersion === 2
+        let newVersion = [ 2, 3 ].includes(pkgLock.lockfileVersion)
           ? pkgLock.packages['node_modules/tiny-json-http'].version
           : pkgLock.dependencies['tiny-json-http'].version
         t.notEqual(newVersion, '7.0.2', `get-index tiny-json-http bumped to ${newVersion} from 7.0.2`)
