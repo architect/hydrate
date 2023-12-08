056ab3c05efb60d586d7eb80625f71552fb8f575
Ryan Block <rblock@gmail.com>
2023-12-08 10:15:42 -0800

    Update get paths unit tests to use `mock-tmp`

--- test/unit/src/shared/get-paths-tests.js
+++ test/unit/src/shared/get-paths-tests.js
@@ -1,17 +1,17 @@
 let test = require('tape')
-let mockFs = require('mock-fs')
+let mockTmp = require('mock-tmp')
 let { join } = require('path')
 let getPaths = require('../../../../src/shared/get-paths')
 let lambdaPath = join('app', 'src', 'http', 'get-items')
-let lambdaPathMockFs = lambdaPath.replace(/\\/g, '/') // mock-fs expects unix paths, even on windows :|
 
 test('inventory with a shared path to hydrate and a single node runtime lambda aliased to a single directory should return a path to node modules', t => {
   t.plan(3)
-  mockFs({ [lambdaPathMockFs]: 'fake file contents' })
+  let tmp = mockTmp({ [lambdaPath]: 'fake file contents' })
+  let path = join(tmp, lambdaPath)
   let inventory = {
     inv: {
       lambdasBySrcDir: {
-        [lambdaPath]: {
+        [path]: {
           name: 'get /items',
           config: { runtime: 'nodejs12' }
         }
@@ -19,23 +19,24 @@ test('inventory with a shared path to hydrate and a single node runtime lambda a
     }
   }
   let paths = getPaths(inventory)
-  t.equals(paths.all[lambdaPath], join(lambdaPath, 'node_modules', '@architect'), 'node runtime lambda returns node_modules path (all)')
+  t.equals(paths.all[path], join(path, 'node_modules', '@architect'), 'node runtime lambda returns node_modules path (all)')
   t.notOk(Object.keys(paths.shared).length, 'Did not load any shared paths')
   t.notOk(Object.keys(paths.views).length, 'Did not load any views paths')
-  mockFs.restore()
+  mockTmp.reset()
 })
 
 test('inventory with a shared path to hydrate and a single non-node runtime lambda aliased to a single directory should return a path to vendor', t => {
   t.plan(3)
   let lambdaPath = join('app', 'src', 'http', 'get-items')
-  mockFs({ [lambdaPathMockFs]: 'fake file contents' })
+  let tmp = mockTmp({ [lambdaPath]: 'fake file contents' })
+  let path = join(tmp, lambdaPath)
   let inventory = {
     inv: {
       shared: {
-        shared: [ lambdaPath ]
+        shared: [ path ]
       },
       lambdasBySrcDir: {
-        [lambdaPath]: {
+        [path]: {
           name: 'get /items',
           config: { runtime: 'python3' }
         }
@@ -43,23 +44,24 @@ test('inventory with a shared path to hydrate and a single non-node runtime lamb
     }
   }
   let paths = getPaths(inventory)
-  t.equals(paths.all[lambdaPath], join(lambdaPath, 'vendor'), 'non-node runtime lambda returns vendor path (all)')
-  t.equals(paths.shared[lambdaPath], join(lambdaPath, 'vendor'), 'non-node runtime lambda returns vendor path (shared)')
+  t.equals(paths.all[path], join(path, 'vendor'), 'non-node runtime lambda returns vendor path (all)')
+  t.equals(paths.shared[path], join(path, 'vendor'), 'non-node runtime lambda returns vendor path (shared)')
   t.notOk(Object.keys(paths.views).length, 'Did not load any views paths')
-  mockFs.restore()
+  mockTmp.reset()
 })
 
 test('inventory with a shared path to hydrate and multiple node runtime lambdas aliased to a single directory should return a single path to node_modules', t => {
   t.plan(3)
   let lambdaPath = join('app', 'src', 'http', 'get-items')
-  mockFs({ [lambdaPathMockFs]: 'fake file contents' })
+  let tmp = mockTmp({ [lambdaPath]: 'fake file contents' })
+  let path = join(tmp, lambdaPath)
   let inventory = {
     inv: {
       views: {
-        views: [ lambdaPath ]
+        views: [ path ]
       },
       lambdasBySrcDir: {
-        [lambdaPath]: [
+        [path]: [
           { name: 'get /items', config: { runtime: 'nodejs12' } },
           { name: 'get /api/v1/items', config: { runtime: 'nodejs12' } },
           { name: 'get /api/v2/items', config: { runtime: 'nodejs12' } },
@@ -68,8 +70,8 @@ test('inventory with a shared path to hydrate and multiple node runtime lambdas 
     }
   }
   let paths = getPaths(inventory)
-  t.equals(paths.all[lambdaPath], join(lambdaPath, 'node_modules', '@architect'), 'node runtime lambda returns node_modules path (all)')
-  t.equals(paths.views[lambdaPath], join(lambdaPath, 'node_modules', '@architect'), 'node runtime lambda returns node_modules path (views)')
+  t.equals(paths.all[path], join(path, 'node_modules', '@architect'), 'node runtime lambda returns node_modules path (all)')
+  t.equals(paths.views[path], join(path, 'node_modules', '@architect'), 'node runtime lambda returns node_modules path (views)')
   t.notOk(Object.keys(paths.shared).length, 'Did not load any shared paths')
-  mockFs.restore()
+  mockTmp.reset()
 })
